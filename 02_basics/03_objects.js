519ba44750194ce797e05727d02d9e05753834a3
Barun Kumar <barun.kumar04@gmail.com>
2023-09-16 14:26:05 +0000

    Obejcts

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -31,7 +31,7 @@ const anotherUser1 = {
 
 // Lets access this.
 console.log(anotherUser1.aSymbol); // Ace - This is correct
-console.log(typeof anotherUser1.aSymbol); // BUT, this is string, it suppose to be a Symobl!!
+console.log(typeof anotherUser1.aSymbol); // BUT, this is string, it suppose to be a Symbol!!
 
 // Correct to declare symbol
 const anotherUser2 = {
@@ -41,7 +41,49 @@ const anotherUser2 = {
     location : "Bengaluru",
     email : "barun@mail.com",
     [aSymbol] : "Ace" // Notice here
+    
 }
 
 console.log(anotherUser2[aSymbol]); // Ace - This is correct
 console.log(typeof anotherUser2[aSymbol]); // Now, this is Symbol, it suppose to be a Symobl!!
+
+// Canging values and freeze
+
+anotherUser2.email = "barunkumar@gmail.com"
+console.log(anotherUser2);
+
+Object.freeze(anotherUser2);
+
+anotherUser2.email = "barunkumar@changedmail.com"
+console.log(anotherUser2); // Not changed, it remained barunkumar@gmail.com
+
+// Method
+
+aUser.greeting = function(){
+    console.log(`Hello ${this["full name"]}`);
+    return ""; // If no return, then an extra undefined will be printed
+}
+
+console.log(aUser.greeting());
+
+// Recap of Object types
+
+// Object nesting
+
+let customer = {
+    id : "1001",
+    name : {
+        "first name" : "Sam",
+        "last name" : "Wilson"
+    },
+    address : {
+        pin : 1234587,
+        location : {
+            xcoordinate : 34.87,
+            ycoordinate : 87.98
+        }
+    }
+};
+
+console.log(customer); // 3 level of nesting
+
