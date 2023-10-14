d9d1791808792ce821d699761cd3a94fbc597d75
Barun Kumar <barun.kumar04@gmail.com>
2023-10-14 16:15:12 +0000

    Advance topics - Object Prop def, stack overflow, lexical and closure

---
+++ 17_advance_concepts/03_defineObjectProperty_makeArrayNonIteratable.js
@@ -0,0 +1,8 @@
+const anArray = [10, 20, 30];
+console.log(typeof anArray);
+console.log(anArray)
+
+const abc = Object.getOwnPropertyDescriptor('', anArray);
+console.log(abc);
+
+// TODO - Seems not possible! Revisit.
